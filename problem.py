0e6eec52478e630022bdf01eea8175cca0e1cba1
Michal Olejnik <kozlakowski48@gmail.com>
2018-02-07 14:01:34 +0100

    added arsenal and tavernposition, basic moves(commit n.2)

--- problem.py
+++ problem.py
@@ -1,12 +1,39 @@
 import time
 
 # TUTAJ ZACZYNAM OPISYWAĆ/TWORZYĆ FUNKCJE UŻYWANE PÓŹNIEJ W GRZE !!!!!!!!!!!!!!!!!!!!!!!!
+
 def starting_position():
     return (" YOUR POSITION\nRight now you're on the 'Rynek Główny', it's your starting position, it's a place full of people\n"
     "You can only go right or left(you see everything from the air perspective:D)\n"
-    "Decide where you want to go, remember you have to find 'Stary Rudzielec', he is somewhere in the city\n")
+    "Decide where do you want to go, remember you have to find 'Stary Rudzielec', he is somewhere in the city\n")
 
+def right_left_move():
+    global right_left
+    right_left = input("Go rigt or left? : ")
+    if right_left == "right":
+        return "So you've moved right!\n"
+    elif right_left == "left":
+        return "So you've moved left!\n"
+    else:
+        return "You should enter yes/no"
 
+def tavern_position():
+    return ("You're in the most famous town's place, it's Tavern.\n"
+    "You can eat and drink here, but be careful, it's full of bad people\n")
+
+def arsenal_position():
+    return ("You're in mysterious place, it's an old arsenal. Usually nobody is here,\n"
+    "but now you see a weird man standing in the corner. You should ask him about 'Stary Rudzielec'\n")
+
+def drink_or_not():
+    print("Now you have to decide.You can stay here, start drinking and have fun or you can\n"
+    "leave this place and go back to the starting position('Rynek Główny') and complete your mission.\n")
+    global drinking_hm
+    drinking_hm = input("So what is you decision?(type stay/leave) : ")
+    if drinking_hm == "stay":
+        return "You've lost the game, your laziness and lack of discipline made you do that"
+    elif drinking_hm == "leave":
+        return "That's a good move, you are going back to the starting position('Rynek Główny')"
 
 
 # TU ZACZYNA SIĘ KOD KONKRETNIE GRY, KOŃCZĄ SIĘ FUNKCJE
@@ -32,6 +59,15 @@ if "yes" in first_decision:
     "Your mission is to find an old guy named 'Stary Rudzielec' and ask him for help. He will know how to make Poland great again!\n")
     # time.sleep(3)
     print(starting_position())
+    print(right_left_move())
+    if right_left == "right":
+        print(tavern_position())
+        print(drink_or_not())
+
+    elif right_left == "left":
+        print(arsenal_position())
+
+
 
 
 
@@ -44,6 +80,6 @@ if "yes" in first_decision:
 
 
 elif "no" in first_decision:
-    for i in range(5):
-        print("Just close this game and don't waste your time kid!!")
-        time.sleep(1)
+    print("JUST CLOSE THIS GAME AND DON'T WASTE YOUR TIME KID!!")
+    time.sleep(3)
+    exit()
